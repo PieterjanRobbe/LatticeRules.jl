b13a96a97cab57cf5002368282836fb7b43b88ec
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-28 14:49:33 +0000

    build based on 78f0d7c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LatticeRules","category":"page"},{"location":"#LatticeRules-1","page":"Home","title":"LatticeRules","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"LatticeRule32\nShiftedLatticeRule32\ngetpoint","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LatticeRules]","category":"page"}]
+}
